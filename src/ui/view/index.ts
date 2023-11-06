b0388ee8073c7e579a8c15179d99bea85c612586
Miguel-Dorta <contact@migueldorta.com>
2023-11-06 10:51:31 +0000

    Integrate UI and Logic

--- src/ui/view/index.ts
+++ src/ui/view/index.ts
@@ -1,14 +1,15 @@
+import { Results } from "../../logic/common/types";
+import { calculate } from "../../logic/function";
 import { hideInput, viewInput } from "./input";
 import { viewOutput } from "./output";
 
 const main = document.getElementsByTagName("main")[0];
 
-export function switchView() {
+export function switchView(r: Results) {
     hideInput(main);
-    viewOutput(main);
+    viewOutput(main, r);
 }
 
-viewInput().then(_ => {
-    // Process data
-    switchView();
+viewInput().then(data => {
+    switchView(calculate(data));
 });
